5b71fc5c2fa914a4b1f06f7b698f799a8b1195e3
Luli91 <cynthiamedina18.08@gmail.com>
2024-10-24 16:02:55 -0300

    topseller

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../home/Banner'
+import TopSellers from '../home/TopSellers'
 
 
 const Home = () => {
     return (
         <>
             <Banner/>
+            <TopSellers/>
         </>
     )
 }
---
+++ src/pages/home/TopSellers.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+
+const categories = ["Elegí la categoria", "Sol", "Lectura", "Computadora", "Deportivos", "Tecnología", "Moda  "]
+
+const TopSellers = () => {
+    const [productos, setProductos] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState ("Elegí la categoria")
+
+    useEffect (() => {
+        fetch("productos.json")
+        .then(res => res.json())
+        .then((data) => setProductos(data))
+    }, [])
+
+    const filteredProductos = selectedCategory === "Elegí la categoria" 
+    ? productos 
+    : productos.filter(producto => producto.category.toLowerCase() === selectedCategory.trim().toLowerCase());
+    console.log(filteredProductos);
+    
+  return (
+    <div className='py-10'>
+        <h2 className='text-3xl font-semibold mb-6'> Top Sellers</h2>
+        {/* category filtering */}
+        <div className='mb-8 flex items-center'>
+            <select
+            onChange={(e) => setSelectedCategory(e.target.value)}
+            name="category" id="category" className='border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none'>
+                {
+                    categories.map((category, index) => (
+                        <option key={index} value={category}>{category}</option>
+                    ))
+                }
+            </select>
+        </div>
+    </div>
+  )
+}
+
+export default TopSellers
